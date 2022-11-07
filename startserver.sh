d12e68db5ac93df9a55ed16e958bd54e88669866
valtteriviirret <viirretvaltteri@gmail.com>
2022-11-08 01:03:51 +0200

    update starserver.sh to accept port number as arg

--- startserver.sh
+++ startserver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-screen -list | grep -q polyblaster
+screen -list | grep -q polyblaster$1
 
 if [[ $? -eq 0 ]]; then
 	echo 'Server is already running!'
 else
 	echo 'Starting server in screen!'
-	screen -dmS polyblaster $HOME/PolyBlaster/server
-	echo 'Open console: screen -r polyblaster'
+	screen -dmS polyblaster$1 $HOME/PolyBlaster/server "$@"
+	echo "Open console: screen -r polyblaster$1"
 fi
 
